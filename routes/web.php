1b8fb2e2afbe796426ba671b1b7cca7db196eb24
MasaKu <masaki_kuyama@yahoo.co.jp>
2020-12-20 00:24:15 +0900

    vessel で環境構築完了

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/test', [TestController::class, 'index']);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
